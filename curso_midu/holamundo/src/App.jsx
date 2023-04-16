11ab89d4dd83b5be04135e1f1bd5e545f85dee14
MatJavier <matia411@gmail.com>
2023-04-16 19:13:43 -0400

    Componente01midu

---
+++ curso_midu/holamundo/src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+const users = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Ángel Durán',
+    isFollowing: true
+  },
+  {
+    userName: 'mat_javier',
+    name: 'Geanny Dominguez.',
+    isFollowing: false
+  },
+ 
+]
+
+export function App () {
+  return (
+    <section className='App'>
+      {
+        users.map(({ userName, name, isFollowing }) => (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        ))
+      }
+    </section>
+  )
+}
---
+++ curso_midu/holamundo/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './App.jsx'
+import './index.css'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+//Los componentes empiezan con mayusculos ejemplo
+
+
+root.render(
+
+<App/>
+
+
+)
